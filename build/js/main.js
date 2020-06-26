094bbd22a9b7cdbd03a84c2154bf73d51e1ee7fc
Олександр Стаднік <sanyokstd@gmail.com>
2020-06-26 13:51:09 +0300

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -172,7 +172,7 @@ $(function(){
 	$(".wrapper .tab").click(function() {
 		if($(window).width() > 767){
 			$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
-			$(".tab_item").hide().eq($(this).index()).fadeIn()
+			$(".tab_item").hide().eq($(this).index()).fadeIn(1000)
 		}
 	}).eq(0).addClass("active");
 
